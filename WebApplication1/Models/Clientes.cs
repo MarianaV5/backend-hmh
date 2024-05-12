26abbb6f4893f6e5a5ed8edea7919ffa3147a82a
Mariana <mariana.2502@hotmail.com>
2024-05-12 00:58:03 -0300

    Mudanças na mensagem das regras de validação.

--- WebApplication1/Models/Clientes.cs
+++ WebApplication1/Models/Clientes.cs
@@ -1,5 +1,9 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
 using System.Runtime.CompilerServices;
 
 namespace TesteProjetoEixo2.Models
@@ -8,19 +12,24 @@ namespace TesteProjetoEixo2.Models
     public class Cliente
     {
         [Key]
+        [Required(ErrorMessage = "Campo Obrigatório. Por favor, preencha o CPF.")]
         public int CPF { get; set; }
-        [Required(ErrorMessage ="Campo Obrigatório!")]
-        public string Nome { get; set; }
-        [Required(ErrorMessage = "Campo Obrigatório!")]
-        public string Email { get; set; }
-        [Required(ErrorMessage = "Campo Obrigatório!")]
-        public int DDD { get; set; }
-        [Required(ErrorMessage = "Campo Obrigatório!")]
-        public int Numero { get; set; }
-        [Required(ErrorMessage = "Campo Obrigatório!")]
-        public DateTime DataNascimento { get; set; }
-        [Required(ErrorMessage = "Campo Obrigatório!")]
-        public string Senha { get; set; }
 
+        [Required(ErrorMessage = "Campo Obrigatório. Por favor, preencha o nome.")]
+        public string Nome { get; set; }
+        
+        [Required(ErrorMessage = "Campo Obrigatório. Por favor, preencha o email.")]
+        public string Email { get; set; }
+        
+        [Required(ErrorMessage = "Campo Obrigatório. Por favor, preencha o DDD.")]
+        public int DDD { get; set; }
+        
+        [Required(ErrorMessage = "Campo Obrigatório. Por favor, preencha o telefone.")]
+        public int Numero { get; set; }
+        [Required(ErrorMessage = "Campo Obrigatório. Por favor, preencha a data de nascimento.")]
+        public DateTime DataNascimento { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigatório. Por favor, informe uma senha.")]
+        public string Senha { get; set; }
     }
 }
